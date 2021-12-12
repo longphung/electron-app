960dd56ac1edca61ee491405f9f2caa98befb8d3
MINH NGUYEN NHAT <minh.nguyennhat@MINHNGUYENNHAT.local>
2021-12-12 16:59:47 +0700

    chore: build electron app in production

--- packages/electron-app/main.ts
+++ packages/electron-app/main.ts
@@ -3,19 +3,21 @@ const path = require("path")
 
 console.log(process.env.NODE_ENV)
 
-const isNotProduction = process.env.NODE_ENV !== "production"
+// const isNotProduction = process.env.NODE_ENV !== "production"
+const isNotProduction = false
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            // preload: path.join(__dirname, 'preload.js'),
+            webSecurity: false,
         }
     })
 
     win.loadURL(
-        isNotProduction ? "http://localhost:3000" : `file://${path.join(__dirname, '../dist/index.html')}`
+        isNotProduction ? "http://localhost:3000" : `file://${path.join(__dirname, '../../../web-frontend/dist/index.html')}`
     )
     win.webContents.openDevTools()
 }
